399d93e37c3c62ae35161c065827fd833b77bdd9
MOUNCIF <Mons-L@users.noreply.github.com>
2020-12-30 14:50:13 +0100

    dxg"frgrbf'bn gtr'vdb

--- app/src/main/java/com/mesmusics/AudioFileManager.java
+++ app/src/main/java/com/mesmusics/AudioFileManager.java
@@ -4,10 +4,14 @@ import android.content.Context;
 import android.database.Cursor;
 import android.media.MediaMetadataRetriever;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 
 
+import java.io.File;
+import java.lang.reflect.Field;
+import java.nio.file.Path;
 import java.util.ArrayList;
 
 
@@ -34,10 +38,33 @@ public class AudioFileManager {
 
 
         Uri uri = Uri.parse("android.resource://com.mesmusics/raw/");
+        File f = new File("android.resource://com.mesmusics/raw/son.wav");
+        Field[] fields = R.raw.class.getFields();
+        for(int count=0; count < fields.length; count++){
+            Log.i("Raw Asset: ", fields[count].getName());
+            AudioFile audioFile = new AudioFile();
+            metadataRetriever.setDataSource(f.getAbsolutePath());
+            String path = metadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+            String name = MediaStore.Audio.AudioColumns.TITLE;
+            String album = MediaStore.Audio.AudioColumns.TITLE;
+            String artist = MediaStore.Audio.AudioColumns.TITLE;
+
+            audioFile.setTitle(name);
+            audioFile.setAlbum(album);
+            audioFile.setArtist(artist);
+            audioFile.setPath(path);
+
+            Log.e("Name :" + name, " Album :" + album);
+            Log.e("Path :" + path, " Artist :" + artist);
+
+            tempAudioFiles.add(audioFile);
+        }
+
+
         String[] projection = {MediaStore.Audio.AudioColumns.DATA, MediaStore.Audio.AudioColumns.TITLE, MediaStore.Audio.AudioColumns.ALBUM, MediaStore.Audio.ArtistColumns.ARTIST};
         Cursor c = context.getContentResolver().query(uri, projection, MediaStore.Audio.Media.DATA + " like ? ", new String[]{"%utm%"}, null);
 
-        if (c != null) {
+     /*   if (c != null) {
             while (c.moveToNext()) {
                 AudioFile audioFile = new AudioFile();
                 String path = c.getString(0);
@@ -56,7 +83,7 @@ public class AudioFileManager {
                 tempAudioFiles.add(audioFile);
             }
             c.close();
-        }
+        }*/
         this.audioFiles = tempAudioFiles;
     }
 }
