5624e47e60fae18e6b92ba8f4b1e3c4cd86e51c8
cerini <enzo.cerini@etu.u-cergy.fr>
2021-01-02 15:36:06 +0100

    Playlist en cours

---
+++ app/src/main/java/com/mesmusics/PlaylistActivity.java
@@ -0,0 +1,13 @@
+package com.mesmusics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PlaylistActivity extends AppCompatActivity {
+
+
+    public void addMusicPlaylist(){
+
+    }
+
+    
+}
