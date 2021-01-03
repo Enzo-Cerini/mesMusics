1fb5eb2174da110a9693ba66b060823b75a9c82a
cerini <enzo.cerini@etu.u-cergy.fr>
2021-01-03 12:46:05 +0100

    Boutton pour retourner à l'accueil depuis AudioInfosActivity

--- app/src/main/java/com/mesmusics/AudioInfosActivity.java
+++ app/src/main/java/com/mesmusics/AudioInfosActivity.java
@@ -1,9 +1,11 @@
 package com.mesmusics;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -14,5 +16,10 @@ public class AudioInfosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio_infos);
+    }
+
+    public void returnToAccueil(View view){
+    Intent myIntent = new Intent(getBaseContext(), MainActivity.class);
+    startActivityForResult(myIntent, 0);
     }
 }
